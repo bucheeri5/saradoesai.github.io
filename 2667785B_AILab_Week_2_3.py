54e7e2f049c33137aa26c648d56df1bb8f482ed1
bucheeri5 <146348947+bucheeri5@users.noreply.github.com>
2023-10-29 10:09:24 +0000

    Add files via upload

---
+++ 2667785B_AILab_Week_2_3.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Week 2: Exploring Data in Multiple Ways
+
+# In[1]:
+
+
+from IPython.display import Image
+
+
+# In[2]:
+
+
+Image ("picture1.jpg")
+
+
+# In[3]:
+
+
+from IPython.display import Audio
+
+
+# In[4]:
+
+
+Audio ("audio1.mid")
+
+
+# In[25]:
+
+
+Audio ("audio2.ogg")
+#This file is licensed under the Creative Commons Attribution-Share Alike 3.0 Unported license. 
+#You are free: to share – to copy, distribute and transmit the workto remix – to adapt the work
+#Under the following conditions: 
+#atribution – You must give appropriate credit, provide a link to the license, and indicate if changes were made. 
+#ou may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.hare alike – If you remix, transform, or build upon the material, you must distribute your contributions under the same or compatible license as the original.
+#The original ogg file was found at the url: https://en.wikipedia.org/wiki/File:GoldbergVariations_MehmetOkonsar-1of3_Var1to10.ogg
+
+
+# The .mid file doesnt work because the file type is Musical Instrument Digital Interface which doesnt carry the audio, and is not supported by the browser, hence why it does not play, unlike the .ogg file. 
+
+# In[26]:
+
+
+from matplotlib import pyplot 
+test_picture = pyplot.imread("picture1.jpg")
+print ("Numpy array of the image is: " , test_picture)
+pyplot.imshow(test_picture)
+test_picture_filtered = 2*test_picture/3
+pyplot.imshow(test_picture_filtered)
+
+
+# In[6]:
+
+
+from sklearn import datasets
+
+
+# In[7]:
+
+
+dir(datasets)
+
+
+# I chose make_moons & load_sample_Image
+
+# In[11]:
+
+
+Moons=datasets.make_moons()
+Sample_Images = datasets.load_sample_image
+Wine_Data = datasets.load_wine()
+
+
+# In[19]:
+
+
+print(Moons)
+
+
+# In[14]:
+
+
+Sample_Images
+
+
+# In[17]:
+
+
+print(Wine_Data.DESCR)
+
+
+# In[21]:
+
+
+from sklearn import datasets
+import pandas
+
+wine_data = datasets.load_wine()
+wine_dataframe = pandas.DataFrame(data=wine_data["data"], columns = wine_data["feature_names"])
+
+
+# In[22]:
+
+
+wine_dataframe.head()
+
+
+# In[23]:
+
+
+wine_dataframe.describe()
+
